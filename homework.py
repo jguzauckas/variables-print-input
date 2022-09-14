fcebae86f81cc97b942ee494a0e20870212f5b0a
jguzauckas <75325154+jguzauckas@users.noreply.github.com>
2022-09-14 07:11:06 -0400

    Created homework.py assignment

---
+++ homework.py
@@ -0,0 +1,14 @@
+#In this assignment, you will create a "fake" conversation with the computer
+#The program will continually respond to what you say and ask pre-programmed questions
+#The responses will incorporate your answers to sound more "realistic"
+
+#For example:
+#Computer: "Hello, what is your name?"
+#You: "Mr. G"
+#Computer: "Hi Mr. G, I am the computer, how old are you?"
+#You: "24"
+#Computer: "Ah, 24 is a great age..."
+
+#Requirements:
+# - Incorporate at least one example of each data type into your inputs (integer, float, boolean, string)
+#   - Since bool feels like an odd one out, treat it like a True/False question!
